ac5e7b2d8cf5ee97f8cefd3ae3b0249e4411fff2
Abdulswamad Rama <abdvswmdr@gmail.com>
2026-05-07 12:55:26 +0300

    add initial Terraform configuration for Azure provider

---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstatedevinitcron"
+    container_name       = "tfstate"
+    key                  = "soqoni/soqoni.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
