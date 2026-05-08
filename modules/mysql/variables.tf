1ce7c083adbb3c956fc18abc844d25679d0d368c
Abdulswamad Rama <abdvswmdr@gmail.com>
2026-05-08 03:06:46 +0300

    add variables for MySQL module configuration

---
+++ modules/mysql/variables.tf
@@ -0,0 +1,45 @@
+variable "server_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "administrator_login" {
+  type = string
+}
+
+variable "administrator_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "sku_name" {
+  type    = string
+  default = "Standard_B1ms"
+}
+
+variable "mysql_version" {
+  type    = string
+  default = "8.0.21"
+}
+
+variable "storage_size_gb" {
+  type    = number
+  default = 32
+}
+
+variable "backup_retention_days" {
+  type    = number
+  default = 7
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
