c03d31512f323001d0c248950ccd60157f357299
Abdulswamad Rama <abdvswmdr@gmail.com>
2026-05-12 14:23:12 +0300

    refactor: add outputs for AKS kubeconfig detailsadd the helm provider (it needs AKS cluster credentials to connect), then add the helm_release resource.

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -10,3 +10,23 @@ output "kubelet_identity" {
   description = "Object ID of the kubelet managed identity — referenced in Key Vault role assignments"
   value       = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
 }
+
+output "cluster_endpoint" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].host
+  sensitive = true
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
