da470c6c97142878fe1cc904b12e31a512bec7cf
Abdulswamad Rama <abdvswmdr@gmail.com>
2026-05-12 13:19:57 +0300

    refactor: add explicit MySQL firewall rules for AKS and dev IP

--- main.tf
+++ main.tf
@@ -56,6 +56,31 @@ resource "azurerm_role_assignment" "kubelet_kv_secrets_user" {
   principal_id         = module.aks.kubelet_identity
 }
 
+# MySQL firewall — explicit allow list replaces the broad "allow Azure services" (0.0.0.0) rule.
+# After applying, manually delete the insecure rules:
+#   az mysql flexible-server firewall-rule delete -g soqoni-rg -n soqoni-mysql --rule-name "AllowAllAzureServicesAndResourcesWithinAzureIps_2026-4-7_16-16-54"
+#   az mysql flexible-server firewall-rule delete -g soqoni-rg -n soqoni-mysql --rule-name "allow-local"
+resource "azurerm_mysql_flexible_server_firewall_rule" "aks_outbound" {
+  name                = "allow-aks-outbound"
+  server_name         = module.mysql.server_name
+  resource_group_name = module.resource_group.name
+  start_ip_address    = "20.255.185.132"
+  end_ip_address      = "20.255.185.132"
+}
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_dev" {
+  name                = "allow-local-ip"
+  server_name         = module.mysql.server_name
+  resource_group_name = module.resource_group.name
+  start_ip_address    = var.dev_ip_address
+  end_ip_address      = var.dev_ip_address
+}
+
+import {
+  to = azurerm_mysql_flexible_server_firewall_rule.allow_dev
+  id = "/subscriptions/fc6b3998-07f2-49a2-b89b-c185f48448c3/resourceGroups/soqoni-rg/providers/Microsoft.DBforMySQL/flexibleServers/soqoni-mysql/firewallRules/allow-local-ip"
+}
+
 import {
   to = module.mysql.azurerm_mysql_flexible_server.this
   id = "/subscriptions/fc6b3998-07f2-49a2-b89b-c185f48448c3/resourceGroups/soqoni-rg/providers/Microsoft.DBforMySQL/flexibleServers/soqoni-mysql"
